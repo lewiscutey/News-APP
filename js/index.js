8bb382522a1c03a89427e618071ec7265dba51f6
lewiscutey <949124363@qq.com>
2016-12-06 10:27:21 +0800

    'b'

--- js/index.js
+++ js/index.js
@@ -30,13 +30,49 @@ $(function () {
             swiperAnimate(swiper); //每个slide切换结束时也运行当前slide动画
         }
 
-});
+    });
 
+//主页菜单导航
+    var navXinwen = $("#nav .xinwen");
+    navXinwen.on("click",function () {
+        $("#nav .item").removeClass("active");
+        $(this).addClass("active");
+        $(".list .menu").css("display","none");
+        $("#index").css("display","block");
+    });
+    var navLuntan = $("#nav .luntan");
+    navLuntan.on("click",function () {
+        $("#nav .item").removeClass("active");
+        $(this).addClass("active");
+        $(".list .menu").css("display","none");
+        $("#luntan").css("display","block");
+    });
+    var navXiaoxi = $("#nav .xiaoxi");
+    navXiaoxi.on("click",function () {
+        $("#nav .item").removeClass("active");
+        $(this).addClass("active");
+        $(".list .menu").css("display","none");
+        $("#xiaoxi").css("display","block");
+    });
+    var navWode = $("#nav .wode");
+    navWode.on("click",function () {
+        $("#nav .item").removeClass("active");
+        $(this).addClass("active");
+        $(".list .menu").css("display","none");
+        $("#wode").css("display","block");
+    });
 
+//内容页面的分享按钮
+    $(".content .nav").hover(function () {
+        $(this).next(".share").stop().slideToggle();
+    });
+    $(".content .pinglun .share").hover(function () {
+        $(".content .title .share").stop().slideToggle();
+    });
 
-
-
-
-
+//内容页面的分享和收藏按钮
+    $(".content .pinglun i").on("click",function () {
+       $(this).css("color","red");
+    });
 
 });
